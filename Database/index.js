bcec6fb0c093e1432b1235044486de475e97bf36
Atulya Miglani <atulyamiglani02@gmail.com>
2023-11-18 22:50:52 -0500

    everything

---
+++ Database/index.js
@@ -0,0 +1,11 @@
+import courses from "./courses.js";
+import assignments from "./assignments.js";
+import modules from "./modules.js";
+export default {
+  courses,
+  modules,
+  assignments,
+  // users,
+  // grades,
+  // enrollments,
+};
